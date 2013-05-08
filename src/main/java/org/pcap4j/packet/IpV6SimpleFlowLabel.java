0764d61a7519d671dd38e3fc0fd0b79f23b0ba3c
kaitoy <kaitoy@pcap4j.org>
2013-05-08 12:29:06 -0600

    fixes to make some compilers happy

--- src/main/java/org/pcap4j/packet/IpV6SimpleFlowLabel.java
+++ src/main/java/org/pcap4j/packet/IpV6SimpleFlowLabel.java
@@ -53,7 +53,7 @@ public final class IpV6SimpleFlowLabel implements IpV6FlowLabel {
   public boolean equals(Object obj) {
     if (obj == this) { return true; }
     if (!this.getClass().isInstance(obj)) { return false; }
-    return (getClass().cast(obj)).value == this.value;
+    return (getClass().cast(obj)).value() == this.value;
   }
 
   @Override
