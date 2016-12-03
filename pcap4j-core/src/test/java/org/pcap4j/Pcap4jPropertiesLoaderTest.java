50574891d39e2af650aab4e632df8f9ab5aec34e
Michael Kötter <michael@m-koetter.de>
2016-12-03 16:49:32 +0100

    improve coverage by using mocks

--- pcap4j-core/src/test/java/org/pcap4j/Pcap4jPropertiesLoaderTest.java
+++ pcap4j-core/src/test/java/org/pcap4j/Pcap4jPropertiesLoaderTest.java
@@ -2,19 +2,58 @@ package org.pcap4j;
 
 import com.sun.jna.Platform;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
-import org.pcap4j.Pcap4jPropertiesLoader;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.pcap4j.util.PropertiesLoader;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.rule.PowerMockRule;
+import org.powermock.reflect.Whitebox;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.pcap4j.Pcap4jPropertiesLoader.PCAP4J_PROPERTIES_PATH_KEY;
 
+@RunWith(Parameterized.class)
+@PrepareForTest(Platform.class)
 public class Pcap4jPropertiesLoaderTest {
 
     private Pcap4jPropertiesLoader propertiesLoader;
 
+    private final int osType;
+
+    @Rule
+    public PowerMockRule powerMockRule = new PowerMockRule();
+
+    public Pcap4jPropertiesLoaderTest(int osType) {
+        this.osType = osType;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> osType() {
+        return Arrays.asList(new Object[][]{
+                {Platform.WINDOWS},
+                {Platform.MAC},
+                {Platform.LINUX},
+                {Platform.FREEBSD},
+                {Platform.OPENBSD}
+        });
+    }
+
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
+        PowerMockito.mockStatic(Platform.class);
+        PowerMockito.when(Platform.getOSType()).thenReturn(osType);
+
         this.propertiesLoader = Pcap4jPropertiesLoader.getInstance();
+
+        Whitebox.setInternalState(propertiesLoader, "loader",
+                new PropertiesLoader("org/pcap4j/pcap4j-test.properties", false, false));
     }
 
     @Test
