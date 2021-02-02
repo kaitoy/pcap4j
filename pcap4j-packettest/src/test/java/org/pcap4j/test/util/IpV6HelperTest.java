e3d1e97748cd4e9e18fe1640da1a2b613aa96cae
Tomas Kunovsky <tkunovsky@seznam.cz>
2021-02-02 21:33:54 +0100

    fix test name

--- pcap4j-packettest/src/test/java/org/pcap4j/test/util/IpV6HelperTest.java
+++ pcap4j-packettest/src/test/java/org/pcap4j/test/util/IpV6HelperTest.java
@@ -26,7 +26,7 @@ public class IpV6HelperTest {
               + ".pcap");
 
   @Test
-  public void testFragmentDefragment() throws Exception {
+  public void testDefragment() throws Exception {
     UnknownPacket.Builder unknownb = new UnknownPacket.Builder();
     unknownb.rawData(new byte[] {0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07});
 
